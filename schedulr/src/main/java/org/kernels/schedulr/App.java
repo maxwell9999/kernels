1e234f1f7caeb52b08d41d0b4321c0c1fbe3ae9d
Courtney Brown <cbrown@pcp134718pcs.wireless.calpoly.edu>
2017-01-30 10:12:16 -0800

    adding maven project

---
+++ schedulr/src/main/java/org/kernels/schedulr/App.java
@@ -0,0 +1,13 @@
+package org.kernels.schedulr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
