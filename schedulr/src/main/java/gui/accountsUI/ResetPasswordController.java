153fb223ce38c70cc79124df0f1f1d5de1056ac3
sarahpadlipsky <Sarah.Padlipsky@gmail.com>
2017-02-08 13:38:44 -0800

    Add error message for new password.

--- schedulr/src/main/java/gui/accountsUI/ResetPasswordController.java
+++ schedulr/src/main/java/gui/accountsUI/ResetPasswordController.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 /**
  * UI for password reset.
@@ -19,20 +19,29 @@ public class ResetPasswordController {
 	@FXML private TextField passwordField;
     @FXML private TextField confirmField;
     @FXML private Button saveButton;
+    @FXML private Label errorLabel;
     
     @FXML
     private void saveButton(ActionEvent event) throws IOException
     {
-    	// Put database code here. 
     	String password = passwordField.getText();
     	String confirmed = confirmField.getText();
     	System.out.println("Password is " + password);
     	System.out.println("Confirmed is " + confirmed);
-    	
-    	Stage stage = (Stage) saveButton.getScene().getWindow();
-        // do what you have to do
-        stage.close();
-    	
+        
+        if (password.equals(confirmed)) {
+        	// Put database code here. 
+        	// To close current window.
+        	Stage stage = (Stage) saveButton.getScene().getWindow();
+            stage.close();
+        
+        } else {
+        	errorLabel.setText("Passwords do not match.");
+        	passwordField.setText("");
+        	confirmField.setText("");
+        }
+
+        // TODO(Sarah): Will also have it open another application and not go back to login screen.
     	
     }
 }
