2ad1a35cba2eba639ef433a06cf90cc9e224b86b
Thomas Simko <tjsimko@gmail.com>
2017-02-13 17:00:31 -0800

    Temp commit

--- schedulr/src/main/java/core/resources/ResourceManager.java
+++ schedulr/src/main/java/core/resources/ResourceManager.java
@@ -1,10 +1,14 @@
 package core.resources;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import core.database.DatabaseCommunicator;
 
@@ -59,6 +63,52 @@ public class ResourceManager
 	public static void removeCourse(String department, int number)
 	{
 		DatabaseCommunicator.deleteDatabase("courses", "department='" + department + "' AND number=" + number + ";");
+	}
+	
+	public static void importCourses(File courseFile) throws FileNotFoundException
+	{
+		Map<String, Object> course = new HashMap<String, Object>();
+		Scanner fileScan = new Scanner(courseFile);
+		Scanner lineScan;
+		String temp;
+		
+		while (fileScan.hasNextLine())
+		{
+			lineScan = new Scanner(fileScan.nextLine());
+			course.put("department", lineScan.next());
+			lineScan.useDelimiter(",");
+			course.put("number", lineScan.next());
+			course.put("name", lineScan.next());
+			course.put("wtu", 0);
+			while (lineScan.hasNext())
+			{
+				temp = lineScan.next();
+				if (temp.contains("lect"))
+				{
+					course.put("lect", temp.substring(0, temp.lastIndexOf(' ')));
+					course.put("wtu", (Integer) course.get("wtu") + lineScan.nextInt());
+				}
+				else if (temp.contains("lab"))
+				{
+					course.put("lab", temp.substring(0, temp.lastIndexOf(' ')));
+					course.put("wtu", (Integer) course.get("wtu") + lineScan.nextInt());
+				}
+				else if (temp.contains("activity"))
+				{
+					course.put("activity", temp.substring(0, temp.lastIndexOf(' ')));
+					course.put("wtu", (Integer) course.get("wtu") + lineScan.nextInt());
+				}
+				else if (temp.contains("supv"))
+				{
+					course.put("supv", temp.substring(0, temp.lastIndexOf(' ')));
+					course.put("wtu", (Integer) course.get("wtu") + lineScan.nextInt());
+				}
+			}
+			lineScan.close();
+			
+		}
+		fileScan.close();
+		
 	}
 
 	/**
