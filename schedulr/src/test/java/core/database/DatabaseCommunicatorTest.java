dc24600ecb9036a00f1b1a59ac69dc389185284e
Courtney Brown <cbrown@pcp134332pcs.wireless.calpoly.edu>
2017-02-22 12:16:55 -0800

    added todo

--- schedulr/src/test/java/core/database/DatabaseCommunicatorTest.java
+++ schedulr/src/test/java/core/database/DatabaseCommunicatorTest.java
@@ -8,6 +8,7 @@ public class DatabaseCommunicatorTest extends TestCase{
 	
 	@Test
 	public void testResourceExists() {
+		//TODO add new user and check if that user exists rather than checking admin
 		assertTrue("Testing admin account exists...", 
 				DatabaseCommunicator.resourceExists("users", "login='admin'")); 
 		assertFalse("Testing monkey account does not exist...", 
