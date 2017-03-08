aae9245cc1bb36fd3498a73e795e16cb17618978
Thomas Simko <tjsimko@gmail.com>
2017-03-08 10:02:41 -0800

    Coverage

--- schedulr/src/main/java/core/resources/Section.java
+++ schedulr/src/main/java/core/resources/Section.java
@@ -96,10 +96,6 @@ public class Section extends Course implements DatabaseObject {
 		return tableName;  
 	}
 	
-	public void addToDatabase() {
-		DatabaseCommunicator.replaceDatabase(this);
-	}
-	
 	@Override
 	public String getKeyIdentifier()
 	{
